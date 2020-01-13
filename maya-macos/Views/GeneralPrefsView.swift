391b3d4a71e44ed5adbd665c25af47cd4d38bf6e
Konstantin Klitenik <kostyan5@gmail.com>
2020-01-13 10:15:56 -0500

    Build prefs view

--- maya-macos/Views/GeneralPrefsView.swift
+++ maya-macos/Views/GeneralPrefsView.swift
@@ -11,24 +11,79 @@ import SwiftUI
 struct GeneralPrefsView: View {
     @State private var openAtLogin = false
     
+    @State private var selection = 0
+    
+    @State private var autoClose = false
+    @State private var autoCloseTimeSelection = 0
+    
+    @State private var autoSwitchPhotos = false
+    @State private var autoSwitchPeriod = 15
+    @State private var autoSwitchPeriodStr = ""
+    @State private var autoSwitchUnitSelection = 0
+    
+    struct HintText: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .font(.system(size: 10))
+                .foregroundColor(.gray)
+        }
+    }
+    
     var body: some View {
-        VStack {
-            HStack {
-                Text("Startup:").frame(alignment: .trailing)
+        VStack(alignment: .leading) {
+            HStack(alignment: .top, spacing: 20) {
+                Text("Startup:").frame(width: 70, alignment: .trailing)
                 VStack(alignment: .leading) {
                     Toggle(isOn: $openAtLogin) {
                         Text("Open Maya at login")
                     }
-                    Text("Start Maya automatically at login.").font(.system(size: 10))
+                    Text("Start Maya automatically at login.").modifier(HintText())
                 }
             }
             Divider().frame(width: 400)
-            HStack {
-                Text("Frame:")
+            HStack(alignment: .top, spacing: 20) {
+                Text("Frame:").frame(width: 70, alignment: .trailing)
+                VStack(alignment: .leading) {
+                    Picker(selection: $selection, label:
+                    Text("When new photos is ready").fixedSize(horizontal: true, vertical: false)) {
+                        Text("one").tag(0)
+//                        Text("two").tag(1)
+                    }.frame(width: 260)
+                    Text("Action to take when new photo is ready.").modifier(HintText())
+                    
+                    HStack {
+                        Toggle(isOn: $autoClose) {
+                            Text("Automatically close after")
+                        }
+                        Picker("", selection: $autoCloseTimeSelection) {
+                            Text("one").tag(0)
+                            Text("two").tag(1)
+                        }.frame(width: 70).padding(.leading, -7)
+                    }
+                    Text("Photo frame will automatically close after this specified period.").modifier(HintText())
+                }
             }
             Divider().frame(width: 400)
-            HStack {
-                Text("Photos")
+            HStack(alignment: .top, spacing: 20) {
+                Text("Photos:").frame(width: 70, alignment: .trailing)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Toggle(isOn: $autoSwitchPhotos) {
+                            Text("Switch photos every").fixedSize()
+                        }
+                        
+                        Stepper(value: $autoSwitchPeriod) {
+                            TextField("", value: $autoSwitchPeriod, formatter: NumberFormatter())
+                                .multilineTextAlignment(.trailing)
+                                .padding(.trailing, -5)
+                        }.frame(width: 50)
+                        Picker("", selection: $autoSwitchUnitSelection) {
+                            Text("one").tag(0)
+                            Text("two").tag(1)
+                        }.frame(width: 70).padding(.leading, -7)
+                    }
+                    Text("New photos will be chosen at this specified period.").modifier(HintText())
+                }
             }
         }.padding()
     }
